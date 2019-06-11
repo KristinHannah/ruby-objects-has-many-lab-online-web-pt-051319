8d3ffc703552592b5914d33952cb6469e8df4277
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-11 03:05:28 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+require "pry"
+
+class Post 
+  attr_accessor :author 
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def title 
+    @title 
+  end 
+  
+  def author_name
+    self.author.name  || self.author 
+  end 
+end 
